da44bacfe6ae54ea0b378a5ec6b4646139e2bd7a
Alex Yang <omega.alex.yang@gmail.com>
2023-12-01 00:26:22 -0800

    Process video and screenshots
    add feature to get the screenshots and cropped image for model training.

--- alex/recognition.py
+++ alex/recognition.py
@@ -1,79 +1,81 @@
-import os
-import cv2
-import torch
-from facenet_pytorch import InceptionResnetV1, MTCNN
-from tqdm import tqdm
-from types import MethodType
-
-
-def encode(img):
-    res = resnet(torch.Tensor(img))
-    return res
-
-
-def detect_box(self, img, save_path=None):
-    # Detect faces
-    batch_boxes, batch_probs, batch_points = self.detect(img, landmarks=True)
-    # Select faces
-    if not self.keep_all:
-        batch_boxes, batch_probs, batch_points = self.select_boxes(
-            batch_boxes, batch_probs, batch_points, img, method=self.selection_method
-        )
-    # Extract faces
-    faces = self.extract(img, batch_boxes, save_path)
-    return batch_boxes, faces
-
-
-# Load model
-resnet = InceptionResnetV1(pretrained='vggface2').eval()
-mtcnn = MTCNN(image_size=224, keep_all=True, thresholds=[0.4, 0.5, 0.5], min_face_size=60)
-mtcnn.detect_box = MethodType(detect_box, mtcnn)
-
-# Get encoded features for all saved images
-saved_pictures = "./celebrity_pic/"
-all_people_faces = {}
-for file in os.listdir(saved_pictures):
-    person_face, extension = os.path.splitext(file)
-    if extension.lower() in ['.jpg', '.png']:
-        img = cv2.imread(f'{saved_pictures}/{file}')
-        cropped = mtcnn(img)
-        if cropped is not None:
-            all_people_faces[person_face] = encode(cropped)[0, :]
-
-
-def detect(cam=0, thres=0.7, video_path=None):
-    if video_path is not None:
-        vdo = cv2.VideoCapture(video_path)
-    else:
-        vdo = cv2.VideoCapture(cam)
-
-    while vdo.grab():
-        _, img0 = vdo.retrieve()
-        batch_boxes, cropped_images = mtcnn.detect_box(img0)
-
-        if cropped_images is not None:
-            for box, cropped in zip(batch_boxes, cropped_images):
-                x, y, x2, y2 = [int(x) for x in box]
-                img_embedding = encode(cropped.unsqueeze(0))
-                detect_dict = {}
-                for k, v in all_people_faces.items():
-                    detect_dict[k] = (v - img_embedding).norm().item()
-                min_key = min(detect_dict, key=detect_dict.get)
-
-                if detect_dict[min_key] >= thres:
-                    min_key = 'Undetected'
-
-                cv2.rectangle(img0, (x, y), (x2, y2), (0, 0, 255), 2)
-                cv2.putText(img0, min_key, (x + 5, y + 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
-
-        # Display
-        cv2.imshow("output", img0)
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    vdo.release()
-    cv2.destroyAllWindows()
-
+# import os
+# import cv2
+# import torch
+# from facenet_pytorch import InceptionResnetV1, MTCNN
+# from tqdm import tqdm
+# from types import MethodType
+#
+# from alex.util import save_screenshots
+#
+#
+# def encode(img):
+#     res = resnet(torch.Tensor(img))
+#     return res
+#
+#
+# def detect_box(self, img, save_path=None):
+#     # Detect faces
+#     batch_boxes, batch_probs, batch_points = self.detect(img, landmarks=True)
+#     # Select faces
+#     if not self.keep_all:
+#         batch_boxes, batch_probs, batch_points = self.select_boxes(
+#             batch_boxes, batch_probs, batch_points, img, method=self.selection_method
+#         )
+#     # Extract faces
+#     faces = self.extract(img, batch_boxes, save_path)
+#     return batch_boxes, faces
+#
+#
+# # Load model
+# resnet = InceptionResnetV1(pretrained='vggface2').eval()
+# mtcnn = MTCNN(image_size=224, keep_all=True, thresholds=[0.4, 0.5, 0.5], min_face_size=60)
+# mtcnn.detect_box = MethodType(detect_box, mtcnn)
+#
+# # Get encoded features for all saved images
+# saved_pictures = "./celebrity_pic/"
+# all_people_faces = {}
+# for file in os.listdir(saved_pictures):
+#     person_face, extension = os.path.splitext(file)
+#     if extension.lower() in ['.jpg', '.png']:
+#         img = cv2.imread(f'{saved_pictures}/{file}')
+#         cropped = mtcnn(img)
+#         if cropped is not None:
+#             all_people_faces[person_face] = encode(cropped)[0, :]
+#
+#
+# def detect(cam=0, thres=0.7, video_path=None):
+#     if video_path is not None:
+#         vdo = cv2.VideoCapture(video_path)
+#     else:
+#         vdo = cv2.VideoCapture(cam)
+#
+#     while vdo.grab():
+#         _, img0 = vdo.retrieve()
+#         batch_boxes, cropped_images = mtcnn.detect_box(img0)
+#
+#         if cropped_images is not None:
+#             for box, cropped in zip(batch_boxes, cropped_images):
+#                 x, y, x2, y2 = [int(x) for x in box]
+#                 img_embedding = encode(cropped.unsqueeze(0))
+#                 detect_dict = {}
+#                 for k, v in all_people_faces.items():
+#                     detect_dict[k] = (v - img_embedding).norm().item()
+#                 min_key = min(detect_dict, key=detect_dict.get)
+#
+#                 if detect_dict[min_key] >= thres:
+#                     min_key = 'Undetected'
+#
+#                 cv2.rectangle(img0, (x, y), (x2, y2), (0, 0, 255), 2)
+#                 cv2.putText(img0, min_key, (x + 5, y + 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+#
+#         # Display
+#         cv2.imshow("output", img0)
+#         if cv2.waitKey(1) == ord('q'):
+#             break
+#
+#     vdo.release()
+#     cv2.destroyAllWindows()
+from alex.util import save_screenshots, process_images, process_videos_in_folder
 
 if __name__ == "__main__":
     # Example usage: python script.py --video_path "path/to/video.mp4"
@@ -89,6 +91,24 @@ if __name__ == "__main__":
     #
     # detect(video_path=args.video_path)
 
-    video_path = 'test.mp4'
-    output_path = './seprated'
-    split_video(video_path, output_path)
+    # video_path = 'test.mp4'
+    # output_path = './seprated'
+    # split_video(video_path, output_path)
+
+    # video_path = './video/F0104.mp4'
+    # output_folder = './screenshots'
+    #
+    # save_screenshots(video_path, output_folder)
+
+    # screenshots_folder = './screenshots'
+    # cropped_folder = './cropped'
+    # results_file = 'my_image_result_file.txt'
+    # cropped_results_file = 'my_cropped_image_result_file.txt'
+    #
+    # process_images(screenshots_folder, cropped_folder, results_file, cropped_results_file)
+    video_folder = './video'  # Folder containing videos
+    screenshots_folder = './screenshots'
+    cropped_folder = './cropped'
+    results_file = 'image_results.txt'
+    cropped_results_file = 'cropped_image_results.txt'
+    process_videos_in_folder(video_folder, screenshots_folder, cropped_folder, results_file, cropped_results_file)
